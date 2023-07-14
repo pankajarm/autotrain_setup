baadb6866204f6c6aa63e0a0dff25afbc7aea6cf
pankajarm <pankymathur@gmail.com>
2023-07-13 20:08:52 -0400

    updated the scripts

--- setup_autotrain.sh
+++ setup_autotrain.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# Step 1: Install necessary packages
+# Step 1: Create a new conda environment
+echo "Creating a new Conda environment..."
+conda create -y -n autotrain_env python=3.9.0
+echo "Conda environment created successfully."
+
+# Step 2: Activate new conda environment
+conda activate autotrain_env
+
+# Step 3: Install necessary packages
 echo "Installing necessary packages..."
 conda install -y cudatoolkit
 conda install -y pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
@@ -20,14 +28,14 @@ conda install -y -c conda-forge hdbscan
 sudo apt-get install --reinstall -y libpq-dev
 pip install tiktoken
 
-# Step 2: Install Git LFS
+# Step 4: Install Git LFS
 echo "Installing Git LFS..."
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt-get install -y git-lfs
 git lfs install
 echo "Git LFS installed successfully."
 
-# Step 3: Install autotrain-advanced
+# Step 5: Install autotrain-advanced
 echo "Installing autotrain-advanced..."
 pip install autotrain-advanced
 echo "Autotrain-advanced installed successfully."
